d76e9bb34d5dc6f92ab46bcf10e4e1ed0fc76e19
Ankur Gupta <ankur11.kmr@gmail.com>
2024-07-03 17:59:07 +0530

    step1: nil return value check + single digit spec addtion

--- spec/add_numbers_spec.rb
+++ spec/add_numbers_spec.rb
@@ -2,9 +2,19 @@ require 'add_numbers'
 
 describe AddNumbers do
   context "add" do
+    it "should return not return nil when empty string" do
+      an = AddNumbers.new
+      expect(an.add("")).not_to be nil
+    end
+
     it "should return zero when empty string" do
       an = AddNumbers.new
       expect(an.add("")).to eq 0
+    end
+
+    it "should return single value as result if given in string" do
+      an = AddNumbers.new
+      expect(an.add("6")).to eq 6
     end
   end
 end
