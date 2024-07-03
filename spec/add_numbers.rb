72bfc44d88d3cb00b7b83f1245fb34bd722e5195
Ankur Gupta <ankur11.kmr@gmail.com>
2024-07-03 18:30:39 +0530

    Step 3: method updates to include \n delimeter requirement

--- spec/add_numbers.rb
+++ spec/add_numbers.rb
@@ -1,6 +1,14 @@
 class AddNumbers
   def add(numbers)
+    # seperation with comma(,)
     array = numbers.split(',')
+
+    # seperation with \n
+    new_array = []
+    array.each do |a|
+      new_array << a.split("\n")
+    end
+    array = new_array.flatten
     return 0 if array.length.zero?
 
     array.map(&:to_i).sum
